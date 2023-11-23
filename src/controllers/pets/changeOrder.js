4d5d34ecd27fb8d92f741e7c9a5c34ed5907571a
Roman lesyo <ladiginscormag@gmail.com>
2023-11-23 09:57:57 +0200

    Update changeOrder.js

--- src/controllers/pets/changeOrder.js
+++ src/controllers/pets/changeOrder.js
@@ -1,31 +1,33 @@
-const { Order } = require('../../schemas/order');
+const { Order } = require('../../schemas/order')
 // const { NotFound } = require("http-errors");
 
 const changeOrder = async (req, res) => {
-   const { descriptionMalfunction, descriptionOfRepair, cost } = req.body;
-   // const { orderId } = req.params;
-   const orderId  = '642e8dbdc1a738bdb26c8091';
+	const { descriptionOfRepair, cost } = req.body
+	const { orderId } = req.params
 
-   try {
-      const result = await Order.findByIdAndUpdate(orderId, {
-         cost,
-         descriptionMalfunction,
-         descriptionOfRepair,
-      });
+	try {
+		const result = await Order.findByIdAndUpdate(orderId, {
+			cost,
+			descriptionOfRepair
+		})
 
-      if (!result) {
-         return res.status(404).json({ status: 'error', message: `Order with id = ${orderId} not found` });
-      }
+		if (!result) {
+			return res.status(404).json({ status: 'error', message: `Order with id = ${orderId} not found` })
+		}
 
-      res.json({
-         status: 'success',
-         code: 200,
-         message: 'Order updated successfully',
-         result,
-      });
-   } catch (error) {
-      res.status(500).json({ status: 'error', message: error.message });
-   }
-};
+		const category = 'прийнятий'
 
-module.exports = changeOrder;
+		const userWithPet = await Order.find({ type: category }).limit(100)
+
+		res.json({
+			status: 'success',
+			code: 200,
+			message: 'Order updated successfully',
+			userWithPet
+		})
+	} catch (error) {
+		res.status(500).json({ status: 'error', message: error.message })
+	}
+}
+
+module.exports = changeOrder
